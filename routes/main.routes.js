1e24566154ff013418bb90eea2f1953d9c3b7886
erdemce <erdemce2234@gmail.com>
2021-02-15 02:16:52 +0100

    add own toys in profile page and routes...

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -3,7 +3,6 @@ const bcrypt = require('bcryptjs');
 const ToyModel = require('../models/Toy.model');
 const UserModel = require('../models/User.model')
 
-//Middleware to protect routes
 const checkLoggedInUser = (req, res, next) => {
   if (req.session.userData) {
     next()
@@ -15,40 +14,29 @@ const checkLoggedInUser = (req, res, next) => {
 
 router.get('/profile', checkLoggedInUser, (req, res, next) => {
   let user = req.session.userData
-  let toyResults
-  ToyModel.find()
-  .then((allToys)=>{
-   toyResults=allToys
+  ToyModel.find({myOwner:user._id})
+  .then((toyResults)=>{
+    let data = {user, toyResults}
+   console.log(toyResults.length)
+   res.render('profile.hbs', {data})
   })
   .catch((err)=>{
     next(err)
   })
-  let data = {user, toyResults}
-  res.render('profile.hbs', {data})
 })
-
 
 router.get('/main', checkLoggedInUser, (req, res, next) => {
   let user = req.session.userData
  ToyModel.find()
   .then((allToys)=>{
     let toyResults=(req.query.searchedToy)?
-    allToys.filter(toy=>toy.name.toLowerCase().includes(req.query.searchedToy.toLowerCase())):allToys
+    allToys.filter(toy=>toy.name.toLowerCase().includes(req.query.searchedToy.toLowerCase())):allToys.filter(item=>item.city==user.city)
      let data = {user, toyResults}
      res.render('main.hbs', {data})
-
   })
   .catch((err)=>{
     next(err)
   })
-
- 
 })
-
-
-
-// router.get('/private', checkLoggedInUser, (req, res, next) => {
-//   res.render('auth/private.hbs')
-// })
 
 module.exports = router
